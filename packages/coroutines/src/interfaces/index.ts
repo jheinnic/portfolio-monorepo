97d29b3295e42430e74548175633ea726d5f8351
John Heinnickel` <jheinnic@gmail.com>
2018-10-31 08:45:52 -0700

    Added SinkLike types

--- packages/coroutines/src/interfaces/index.ts
+++ packages/coroutines/src/interfaces/index.ts
@@ -2,3 +2,4 @@ export * from './limitter.type';
 export * from './message-for-ack.interface';
 export * from './message-for-reply.interface';
 export * from './concurrent-work-factory.interface';
+export * from './sink-like.type';
