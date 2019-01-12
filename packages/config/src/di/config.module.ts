2b693b183fb675efbf7cc518c200f1e580218a3b
John Heinnickel` <jheinnic@gmail.com>
2019-01-11 19:30:10 -0800

    Formatting for tslint

--- packages/config/src/di/config.module.ts
+++ packages/config/src/di/config.module.ts
@@ -1,14 +1,12 @@
-import {DynamicModule, Global, Module, Provider} from '@nestjs/common';
-import {toArray} from 'rxjs/operators';
+import { DynamicModule, Global, Module, Provider } from '@nestjs/common';
+import { toArray } from 'rxjs/operators';
 
-import {IConfigClassFinder} from '../interfaces/config-class-finder.interface';
-import {IConfigProviderFactory} from '../interfaces';
-import {ConfigProviderFactoryService} from '../config-provider-factory.service';
-import {ConfigClassFinderService} from '../config-class-finder.service';
-import {
-   CONFIG_CLASS_FINDER_PROVIDER, CONFIG_FILE_READER_PROVIDER, CONFIGURATION_FACTORY_PROVIDER
-} from './config.constants';
-import {ConfigLoader} from '../config-loader.service';
+import { IConfigClassFinder } from '../interfaces/config-class-finder.interface';
+import { IConfigProviderFactory } from '../interfaces';
+import { ConfigProviderFactoryService } from '../config-provider-factory.service';
+import { ConfigClassFinderService } from '../config-class-finder.service';
+import { CONFIGURATION_FACTORY_PROVIDER } from './config.constants';
+import { ConfigLoader } from '../config-loader.service';
 
 @Global()
 @Module({ })
@@ -37,20 +35,12 @@ export class ConfigModule {
          imports: [ ],
          providers: [
             {
-            //    provide: CONFIG_FILE_READER_PROVIDER,
-            //    useValue: configFileReader
-            // }, {
-            //    provide: CONFIG_CLASS_FINDER_PROVIDER,
-            //    useValue: configClassFinder
-            // }, {
                provide: CONFIGURATION_FACTORY_PROVIDER,
                useValue: configFactory
             },
             ...configProviders
          ],
          exports: [
-            CONFIG_FILE_READER_PROVIDER,
-            CONFIG_CLASS_FINDER_PROVIDER,
             CONFIGURATION_FACTORY_PROVIDER,
             ... configProviders
          ]
