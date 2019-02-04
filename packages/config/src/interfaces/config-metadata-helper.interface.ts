25f73c1ef47a2b89686d431b2990b30cc4fa006e
John Heinnickel` <jheinnic@gmail.com>
2019-02-03 20:52:56 -0800

    Added DI utilities for facilitating a common pattern in asynchronous Nest module definition

---
+++ packages/config/src/interfaces/config-metadata-helper.interface.ts
@@ -0,0 +1,21 @@
+import {ConstructorFor} from 'simplytyped';
+import { MetadataMap } from '@loopback/metadata';
+
+import { ProviderToken } from '@jchptf/api';
+import { ConfigPropertyMarker } from '../decorators/config-property-marker.interface';
+
+export interface IConfigMetadataHelper
+{
+   hasConfigMetadata<T extends object>(configClass: ConstructorFor<T>): boolean;
+
+   hasProviderToken<T extends object>(configClass: ConstructorFor<T>): boolean;
+
+   getProviderToken<T extends object>(configClass: ConstructorFor<T>): ProviderToken<T>;
+
+   getPropertyRoot<T extends object>(configClass: ConstructorFor<T>): string;
+
+   getPropertyMetadata<T extends object>(
+      configClass: ConstructorFor<T>): MetadataMap<ConfigPropertyMarker>;
+
+   getPropertyDesignType<T extends object>(sampleInst: T, propName: string): any;
+}
