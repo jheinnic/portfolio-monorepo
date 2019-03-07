6f401b3474eef7dadcba8fac3e7b96cdcfe26a18
John Heinnickel` <jheinnic@gmail.com>
2019-03-07 02:32:07 -0800

    Importing "events.EventEmitter" rather than attemptign to use global NodeJS namespace

--- packages/consul/src/di/consul.module.ts
+++ packages/consul/src/di/consul.module.ts
@@ -5,6 +5,7 @@ import {
    CONSUL_CLIENT_PROVIDER, CONSUL_EVENT_EMITTER_PROVIDER, CONSUL_OPTIONS_PROVIDER,
 } from './consul.constants';
 import { AsyncModuleParam, asyncProviderFromParam } from '@jchptf/api';
+import { EventEmitter } from 'events';
 
 @Global()
 @Module({})
@@ -17,7 +18,7 @@ export class ConsulModule
       const consulProvider = {
          provide: CONSUL_CLIENT_PROVIDER,
          useFactory: async (
-            consulOptions: ConsulOptions, emitter: NodeJS.EventEmitter,
+            consulOptions: ConsulOptions, emitter: EventEmitter,
          ): Promise<IConsul> => {
             // if (!get(consulOptions, 'defaults.timeout')) {
             //    set(consulOptions, 'defaults.timeout', 5000);
@@ -37,8 +38,8 @@ export class ConsulModule
 
       const consulEventEmitterProvider = {
          provide: CONSUL_EVENT_EMITTER_PROVIDER,
-         useFactory: async (): Promise<NodeJS.EventEmitter> => {
-            return new NodeJS.EventEmitter();
+         useFactory: async (): Promise<EventEmitter> => {
+            return new EventEmitter();
          },
       };
 
