76c4296eab4bf92d2473b20d6154fa5e1a7d0c7d
John Heinnickel` <jheinnic@gmail.com>
2019-03-09 14:10:31 -0800

    Adding extra NamedTypeIntents for Monitors and AsyncSinks

--- packages/coroutines/src/di/coroutines.constants.ts
+++ packages/coroutines/src/di/coroutines.constants.ts
@@ -7,11 +7,13 @@ import { IConcurrentWorkFactory } from '../interfaces';
 import { Chan } from 'medium';
 import { IAdapter } from '@jchptf/api';
 import { ModuleChannelConfig } from './model/module-channel-config.type';
+import { IChanMonitor } from '../interfaces/chan-monitor.interface';
+import { AsyncSink } from 'ix';
 
 export const COROUTINES_MODULE =
    getModuleIdentifier('@jchptf/coroutines');
 
-const CWF =
+const CONTENT_WORK_FACTORY =
    getNamedTypeIntent<IConcurrentWorkFactory>('IConcurrentWorkFactory');
 
 export const MODULE_CHANNEL_CONFIG =
@@ -20,11 +22,17 @@ export const MODULE_CHANNEL_CONFIG =
 export const CHAN_TYPE =
    getNamedTypeIntent<IAdapter<Chan<any, any>>>('IAdapter<Chan<any, any>>');
 
+export const MONITOR_TYPE =
+   getNamedTypeIntent<IChanMonitor<any>>('IChanMonitor<any>');
+
+export const ASYNC_SINK_TYPE =
+   getNamedTypeIntent<AsyncSink<any>>('AsyncSink<any>');
+
 export const CONCURRENT_WORK_FACTORY =
-   getLocalProviderToken<IConcurrentWorkFactory>(COROUTINES_MODULE, CWF);
+   getLocalProviderToken<IConcurrentWorkFactory>(COROUTINES_MODULE, CONTENT_WORK_FACTORY);
 
 export const MODULE_CHANNEL_CONFIG_TOKEN_PROVIDER =
    getLocalProviderToken<ModuleChannelConfig<any>>(COROUTINES_MODULE, MODULE_CHANNEL_CONFIG);
 
 export const COROUTINE_DYNAMIC_CHANNELS_BINDING =
-   getDynamicProviderBinding(COROUTINES_MODULE, "ChanBinding");
+   getDynamicProviderBinding(COROUTINES_MODULE, 'ChanBinding');
