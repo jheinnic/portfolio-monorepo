750f5ecd3a770ad72c9b54fc9c1fdc37bc24f79e
John Heinnickel` <jheinnic@gmail.com>
2019-03-10 11:12:15 -0700

    Using IResourceAdapter in favor of ResourceAdapter as per best practice
    Using the class with ProviderTokens is often unnecessary and can be of
    higher risk for creating a circular dependency, since ProviderTokens are
    used in implementation classes but not in interfaces.

--- packages/semaphore/src/di/resource-semaphore.constants.ts
+++ packages/semaphore/src/di/resource-semaphore.constants.ts
@@ -5,7 +5,6 @@ import {
    getNamedTypeIntent, NAMED_EITHER_ITERABLE_ANY_TYPE,
    TypeIdentifier
 } from '@jchptf/nestjs';
-import {ResourceAdapter} from '../resource-adapter.class';
 import {
    IResourceAdapter, IResourceSemaphore, IResourceSemaphoreFactory
 } from '../interfaces';
@@ -23,10 +22,10 @@ export const SEMAPHORE_RESOURCE_POOL_PROVIDER_TOKEN =
    getLocalProviderToken(SEMAPHORE_MODULE_ID, NAMED_EITHER_ITERABLE_ANY_TYPE);
 
 export const RESERVATIONS_CHANNEL_TYPE: TypeIdentifier<Chan<IResourceAdapter<any>, any>> =
-   getNamedTypeIntent<Chan<ResourceAdapter<any>, any>>("Chan<ResourceAdapter<T>, T>");
+   getNamedTypeIntent<Chan<IResourceAdapter<any>, any>>("Chan<IResourceAdapter<T>, T>");
 
 export const RETURN_CHANNEL_TYPE: TypeIdentifier<Chan<any, IResourceAdapter<any>>> =
-   getNamedTypeIntent<Chan<any, ResourceAdapter<any>>>("Chan<T, ResourceAdapter<T>>");
+   getNamedTypeIntent<Chan<any, IResourceAdapter<any>>>("Chan<T, IResourceAdapter<T>>");
 
 export const SEMAPHORE_SERVICE_TYPE =
    getNamedTypeIntent<IResourceSemaphore<any>>("IResourceSemaphore<T>")
