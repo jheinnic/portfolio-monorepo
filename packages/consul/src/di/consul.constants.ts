f977e4000a96135f51afe6dacad70daaa672da65
John Heinnickel` <jheinnic@gmail.com>
2019-03-10 11:07:35 -0700

    Formatting line lengths for tslint

--- packages/consul/src/di/consul.constants.ts
+++ packages/consul/src/di/consul.constants.ts
@@ -10,9 +10,12 @@ const IConsul = getNamedTypeIntent<Consul>('Consul');
 const ConsulOptions = getNamedTypeIntent<ConsulOptions>('ConsulOptions');
 const EventEmitter = getNamedTypeIntent<NodeJS.EventEmitter>('EventEmitter');
 
-export const CONSUL_CLIENT_PROVIDER = getGlobalProviderToken<Consul>(IConsul);
-export const CONSUL_OPTIONS_PROVIDER = getLocalProviderToken<ConsulOptions>(CONSUL_MODULE_ID, ConsulOptions);
-export const CONSUL_EVENT_EMITTER_PROVIDER = getLocalProviderToken<NodeJS.EventEmitter>(CONSUL_MODULE_ID, EventEmitter);
+export const CONSUL_CLIENT_PROVIDER =
+   getGlobalProviderToken<Consul>(IConsul);
+export const CONSUL_OPTIONS_PROVIDER =
+   getLocalProviderToken<ConsulOptions>(CONSUL_MODULE_ID, ConsulOptions);
+export const CONSUL_EVENT_EMITTER_PROVIDER =
+   getLocalProviderToken<NodeJS.EventEmitter>(CONSUL_MODULE_ID, EventEmitter);
 
 export const CONSUL_SERVICE_DYNAMIC_MODULE_TYPE =
    getDynamicModuleType(CONSUL_MODULE_ID, 'ServicesDynamicModule');
