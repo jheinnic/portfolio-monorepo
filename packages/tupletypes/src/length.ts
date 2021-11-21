84bfe1ca071b56e9a4e88048acec7533ba508638
John Heinnickel <john.heinnickel@thermofisher.com>
2021-11-21 04:32:21 -0800

    Fix type restriction compilation problem with Length Of

--- packages/tupletypes/src/length.ts
+++ packages/tupletypes/src/length.ts
@@ -5,22 +5,24 @@ export type SupportedLength = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 
 export type SupportedIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
 
 export type LengthOf<T extends any[]> =
-   T extends { length: infer N } ? N & SupportedLength : never;
+   T extends { length: infer N }
+       ? (N extends SupportedLength ? N : never)
+       : never;
 
 type IndexTypes = [
    never,
    0,
-       0 | 1,
-       0 | 1 | 2,
-       0 | 1 | 2 | 3,
-       0 | 1 | 2 | 3 | 4,
-       0 | 1 | 2 | 3 | 4 | 5,
-       0 | 1 | 2 | 3 | 4 | 5 | 6,
-       0 | 1 | 2 | 3 | 4 | 5 | 6 | 7,
-       0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8,
-       0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9,
-       0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10,
-       0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
+   0 | 1,
+   0 | 1 | 2,
+   0 | 1 | 2 | 3,
+   0 | 1 | 2 | 3 | 4,
+   0 | 1 | 2 | 3 | 4 | 5,
+   0 | 1 | 2 | 3 | 4 | 5 | 6,
+   0 | 1 | 2 | 3 | 4 | 5 | 6 | 7,
+   0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8,
+   0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9,
+   0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10,
+   0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
 ];
 
 type InsertIndexTypes = [
@@ -36,7 +38,7 @@ type InsertIndexTypes = [
    0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9,
    0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10,
    0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11,
-   never
+   0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12,
 ];
 
 export type IndicesByLength<N extends SupportedLength> = IndexTypes[N];
